d53890ab667975e5b1d123c8364402215843ae9c
Peter Vertenten <peter.vertenten@gmail.com>
2018-07-12 15:31:12 -0400

    cleanup

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -5,20 +5,10 @@ import javax.inject._
 import play.api.mvc._
 import org.apache.spark.sql.SparkSession
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents, spark: SparkSession) (implicit assetsFinder: AssetsFinder)
   extends AbstractController(cc) {
 
-  /**
-   * Create an Action to render an HTML page with a welcome message.
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
