919b93e848d59751c615b6f490a2c2414fe129fe
Peter Vertenten <peter.vertenten@gmail.com>
2018-07-12 23:01:04 -0400

    new streams package

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -2,10 +2,14 @@ package controllers
 
 import javax.inject._
 
+import java.io.File
 import play.api.mvc._
 import play.api.libs.iteratee.Enumerator
 import org.apache.spark.sql.SparkSession
 import play.http.HttpEntity
+import scala.concurrent.ExecutionContext.Implicits.global
+import akka.stream.scaladsl.Source
+import play.api.libs.streams.Streams
 
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents, sparkSession: SparkSession) (implicit assetsFinder: AssetsFinder)
@@ -18,12 +22,9 @@ class HomeController @Inject()(cc: ControllerComponents, sparkSession: SparkSess
 
   def data = Action {
     val df = sparkSession.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("data/eth.csv")
-    val enum   = Enumerator.fromFile("data/eth.csv")
-    val source = akka.stream.scaladsl.Source.fromPublisher(play.api.libs.streams.Streams.enumeratorToPublisher(enum))
-    Result(
-      header = ResponseHeader(OK, Map(CONTENT_DISPOSITION -> "attachment; filename=data.csv")),
-      body = HttpEntity.Streamed(source, None, None)
-    )
+    val enumerator = Enumerator.fromFile(new File("data/eth.csv"))
+    val source = Source.fromPublisher(Streams.enumeratorToPublisher(enumerator))
+    Ok.sendEntity(HttpEntity.Streamed(source, None, Some("text/csv")))
   }
 
 }
