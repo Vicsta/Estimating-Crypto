bf46c8252befed1bca971a295a80fa235c3e5f12
Peter Vertenten <peter.vertenten@gmail.com>
2018-07-12 22:41:18 -0400

    Adding streaming support libraries

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -3,7 +3,9 @@ package controllers
 import javax.inject._
 
 import play.api.mvc._
+import play.api.libs.iteratee.Enumerator
 import org.apache.spark.sql.SparkSession
+import play.http.HttpEntity
 
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents, sparkSession: SparkSession) (implicit assetsFinder: AssetsFinder)
@@ -16,7 +18,12 @@ class HomeController @Inject()(cc: ControllerComponents, sparkSession: SparkSess
 
   def data = Action {
     val df = sparkSession.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("data/eth.csv")
-    Ok(views.html.index(df))
+    val enum   = Enumerator.fromFile("data/eth.csv")
+    val source = akka.stream.scaladsl.Source.fromPublisher(play.api.libs.streams.Streams.enumeratorToPublisher(enum))
+    Result(
+      header = ResponseHeader(OK, Map(CONTENT_DISPOSITION -> "attachment; filename=data.csv")),
+      body = HttpEntity.Streamed(source, None, None)
+    )
   }
 
 }
