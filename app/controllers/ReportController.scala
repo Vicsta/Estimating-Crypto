18ca1d73e80604b809ec0a93b57102bebca1fd3a
Peter Vertenten <peter.vertenten@gmail.com>
2018-07-13 10:13:55 -0400

    pulling back specific pair

--- app/controllers/ReportController.scala
+++ app/controllers/ReportController.scala
@@ -24,18 +24,16 @@ class ReportController @Inject()(cc: ControllerComponents, sparkSession: SparkSe
     Ok(views.html.index(df))
   }
 
-  def data = Action {
-    val df = sparkSession.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("data/eth.csv")
-    val file = new java.io.File("data/eth.csv")
-    val path = file.toPath();
-    val source: Source[ByteString, _] = StreamConverters.fromInputStream(()=> getData(df))
+  def data(pair: String) = Action {
+    val source: Source[ByteString, _] = StreamConverters.fromInputStream(()=> getData(pair))
     Result(
         header = ResponseHeader(200, Map.empty),
         body =  HttpEntity.Streamed(source, None, Some("text/csv"))
     )
   }
 
-  def getData(df: org.apache.spark.sql.DataFrame) : InputStream = {
+  def getData(pair: String) : InputStream = {
+    val df = sparkSession.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("data/" + pair + ".csv")
     val out = new ByteArrayOutputStream()
     val writer = new PrintWriter(new OutputStreamWriter(out))
     val header = df.columns.mkString(",")
