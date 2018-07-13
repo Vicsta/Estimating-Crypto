7864b45c13eea66b7614c8419e588743c47f6bfb
Peter Vertenten <peter.vertenten@gmail.com>
2018-07-12 23:44:03 -0400

    pulling dynamic

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -6,10 +6,13 @@ import java.io.File
 import play.api.mvc._
 import play.api.libs.iteratee.Enumerator
 import org.apache.spark.sql.SparkSession
-import play.http.HttpEntity
+import play.api.http.HttpEntity
 import scala.concurrent.ExecutionContext.Implicits.global
-import akka.stream.scaladsl.Source
-import play.api.libs.streams.Streams
+import akka.stream.scaladsl.{ FileIO, Source }
+import play.api.libs.iteratee.streams.IterateeStreams
+import play.api.libs.ws._
+import play.api.libs.iteratee._
+import akka.util.ByteString
 
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents, sparkSession: SparkSession) (implicit assetsFinder: AssetsFinder)
@@ -22,9 +25,13 @@ class HomeController @Inject()(cc: ControllerComponents, sparkSession: SparkSess
 
   def data = Action {
     val df = sparkSession.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("data/eth.csv")
-    val enumerator = Enumerator.fromFile(new File("data/eth.csv"))
-    val source = Source.fromPublisher(Streams.enumeratorToPublisher(enumerator))
-    Ok.sendEntity(HttpEntity.Streamed(source, None, Some("text/csv")))
+    val file = new java.io.File("data/eth.csv")
+    val path = file.toPath();
+    val source: Source[ByteString, _] = FileIO.fromPath(path)
+    Result(
+        header = ResponseHeader(200, Map.empty),
+        body =  HttpEntity.Streamed(source, None, Some("text/csv"))
+    )
   }
 
 }
