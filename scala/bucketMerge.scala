d38ce8bb4c34a7e8518bfe161aaa858fb068199a
John Reynolds <john@Johns-iMac-2.local>
2018-07-21 12:57:36 -0400

    added price differential columns, drop unnneded columns

--- scala/bucketMerge.scala
+++ scala/bucketMerge.scala
@@ -7,7 +7,7 @@ import org.apache.spark.sql._
 
 case class Trade(price: Float, qty: Float, time: Long, bucket: Long)
 
-def createDf(dataDir: String, pair : String) = {
+def createDf(dataDir: String, pair : String, keepFields : Array[String]) = {
 
     val r = sc.textFile(dataDir + pair + ".csv")
     .filter(_.split(",")(0) != "price")
@@ -45,17 +45,26 @@ def createDf(dataDir: String, pair : String) = {
      val lagFwd = org.apache.spark.sql.expressions.Window.orderBy("bucket").rowsBetween(0,10)
      df = df.withColumn("ma_fwd", avg(df("price")).over(lagFwd))
 
+     // create column which is difference between current price and backward moving average
+     df = df.withColumn("dpx", df("price") - df("ma_back"))
+
+     // create column wihic is differente between fwd moving average and current price
+     df = df.withColumn("fwdDelta", df("ma_fwd") - df("price"))
+
      val fields = df.schema.fields.toList
      fields.foreach((field) => {
-        if(!field.name.equals("bucket")) {
-	  df = df.withColumnRenamed(field.name, pair + "_" + field.name)
-	}
+        if(!keepFields.contains(field.name)) {
+	  df = df.drop(df.col(field.name))
+        }
+        else if(!field.name.equals("bucket")) {
+	   df = df.withColumnRenamed(field.name, pair + "_" + field.name)
+        }
      })
      df	
 }
 
-def merge(dataDir: String, pairs : Array[String]) : org.apache.spark.sql.DataFrame = {
-    val frames = pairs.map(p => (p,createDf(dataDir, p)))
+def merge(dataDir: String, pairs : Array[String], keepFields : Array[String]) : org.apache.spark.sql.DataFrame = {
+    val frames = pairs.map(p => (p,createDf(dataDir, p, keepFields)))
     val head = frames.head._1
     var merged = frames.head._2
     frames.foreach(pair => {
@@ -69,9 +78,10 @@ def merge(dataDir: String, pairs : Array[String]) : org.apache.spark.sql.DataFra
 }
 
 val pairs : Array[String] = Array("XRP", "ETH", "XBT", "LTC")
+val keepFields : Array[String] = Array("bucket", "price", "dpx", "fwdDelta")
 val dataDir : String = "hdfs:///user/jr4716/bucket/"
 
-val merged = merge(dataDir, pairs)
+val merged = merge(dataDir, pairs, keepFields)
 
 
 
